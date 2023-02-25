60516546ac7824fb33166d75487267115d5b9adb
Andreas Schmid <andreas.schmid@ur.de>
2023-02-25 18:48:44 +0100

    add simple frontend that displays the content of one CD

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+include("get_data.php");
+
+?>
+
+<html>
+
+<head>
+  <title>Bravo Hits Hits</title>
+  <link rel="stylesheet" href="css/style.css">
+  <!-- Add favicon ico -->
+  <!--<link rel="icon" href="img/favicon.ico" type="image/x-icon" />-->
+</head>
+
+<body>
+<table class="track-list">
+
+<?php
+
+$counter = 1;
+
+foreach($tracks as $track)
+{
+    echo "<tr class=\"track-row\">";
+    echo "<td class=\"index-cell\">";
+    echo $counter;
+    echo "</td>";
+    echo "<td class=\"artist-cell\">";
+    echo $track[$INDEX_ARTIST];
+    echo "</td>";
+    echo "<td class=\"track-cell\">";
+    echo $track[$INDEX_TRACKNAME];
+    echo "</td>";
+    echo "</tr>";
+    $counter++;
+}
+
+//var_dump($tracks)
+?>
+
+</table>
+
+</body>
